04dcd407d1b701d979b1409839681ec60ac65a06
Bri Ward <me@briward.com>
2025-12-26 11:57:55 +0000

    Use seperate deno.publish config when publishing to JSR to help with type checks.

--- src/framework-types.ts
+++ src/framework-types.ts
@@ -1,10 +1,22 @@
-// Shim, used only for type-check during publishing to JSR.
-
 import type { Params } from "./interfaces/params.ts";
+
+/**
+ * Type shim for @raptor/framework.
+ * This allows module augmentation to work during JSR publish.
+ */
 
 export interface Context {
   request: Request;
+  response: Response;
   params: Params;
 }
 
-export declare class NotFound extends Error {}
+export declare class NotFound extends Error {
+  constructor(message?: string);
+}
+
+export declare class Kernel {
+  constructor();
+  add(handler: (context: Context) => unknown): void;
+  respond(request: Request): Promise<Response>;
+}
